223ee2219d367849ba97541dbe180031686f6fd7
Ed O'Connor <ed@oconnor.app>
2022-07-01 13:51:41 -0400

    renamed main to app

--- app/app.py
+++ app/app.py
@@ -5,16 +5,16 @@ from . import db
 from .models import Blogpost
 from datetime import datetime
 
-main = Blueprint('main', __name__)
+app = Blueprint('app', __name__)
 
-@main.route("/")
+@app.route("/")
 def index():
     date = datetime.now()
     blogposts = Blogpost.query.order_by(Blogpost.date_posted.desc()).all()
     return render_template("index.html", blogposts=blogposts, date=date)
 
 
-@main.route('/<int:blogpost_id>/')
+@app.route('/<int:blogpost_id>/')
 def blogpost(blogpost_id):
     blogpost = Blogpost.query.get_or_404(blogpost_id)
     return render_template('blogpost.html', blogpost=blogpost)
