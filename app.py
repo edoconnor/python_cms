62d5f374b47d8b77695dbb7996bbebc4ba4a3eb9
Ed O'Connor <ed@oconnor.app>
2022-07-01 18:57:40 -0400

    changed everything

--- app.py
+++ app.py
@@ -1,21 +1,60 @@
-from flask_login import login_user, login_required, logout_user, current_user
-from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask import Flask
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
-from .models import User, Blogpost
+from flask import render_template, redirect, url_for, request, flash
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from sqlalchemy import func
 
-from . import db
+db = SQLAlchemy()
 
-auth = Blueprint('auth', __name__)
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '826DECC6B5FC656D4A193E3B8C5D3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
 
 
-@auth.route('/signup')
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+
+
+class Blogpost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    subtitle = db.Column(db.String(50))
+    author = db.Column(db.String(20))
+    content = db.Column(db.Text)
+    image = db.Column(db.String(100))
+    date_posted = db.Column(db.DateTime(timezone=True),
+                            server_default=func.now())
+
+    def __repr__(self):
+        return f"<Blogpost {self.id}>"
+
+
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.route('/signup')
 @login_required
 def signup():
     return render_template('signup.html')
 
 
-@auth.route('/signup', methods=['POST'])
+@app.route('/signup', methods=['POST'])
 @login_required
 def signup_post():
 
@@ -27,7 +66,7 @@ def signup_post():
 
     if user:
         flash('Email address already exists!')
-        return redirect(url_for('auth.signup'))
+        return redirect(url_for('signup'))
 
     new_user = User(email=email, name=name,
                     password=generate_password_hash(password, method='sha256'))
@@ -35,22 +74,22 @@ def signup_post():
     db.session.add(new_user)
     db.session.commit()
 
-    return redirect(url_for('auth.login'))
+    return redirect(url_for('login'))
 
 
-@auth.route('/profile')
+@app.route('/profile')
 @login_required
 def profile():
     flash('You are logged in!', 'success')
     return render_template('profile.html', name=current_user.name)
 
 
-@auth.route('/login')
+@app.route('/login')
 def login():
     return render_template('login.html')
 
 
-@auth.route('/login', methods=['POST'])
+@app.route('/login', methods=['POST'])
 def login_post():
 
     email = request.form.get('email')
@@ -61,22 +100,22 @@ def login_post():
 
     if not user or not check_password_hash(user.password, password):
         flash('Please try again.', 'error')
-        return redirect(url_for('auth.login'))
+        return redirect(url_for('login'))
 
     elif user and check_password_hash(user.password, password):
         flash('You are logged in!', 'success')
         login_user(user, remember=remember)
 
-    return redirect(url_for('auth.profile'))
+    return redirect(url_for('profile'))
 
 
-@auth.route('/create')
+@app.route('/create')
 @login_required
 def create():
     return render_template('create.html')
 
 
-@auth.route('/create', methods=['POST'])
+@app.route('/create', methods=['POST'])
 @login_required
 def create_post():
 
@@ -95,10 +134,10 @@ def create_post():
     db.session.add(blogpost)
     db.session.commit()
 
-    return redirect(url_for('app.index'))
+    return redirect(url_for('index'))
 
 
-@auth.route('/edit/<int:blogpost_id>/', methods=('GET', 'POST'))
+@app.route('/edit/<int:blogpost_id>/', methods=('GET', 'POST'))
 @login_required
 def edit_post(blogpost_id):
     blogpost = Blogpost.query.get_or_404(blogpost_id)
@@ -119,22 +158,39 @@ def edit_post(blogpost_id):
         db.session.add(blogpost)
         db.session.commit()
 
-        return redirect(url_for("app.index"))
+        return redirect(url_for("index"))
 
     return render_template('edit.html', blogpost=blogpost)
 
 
-@auth.route("/delete/<int:blogpost_id>/", methods=['POST'])
+@app.route("/delete/<int:blogpost_id>/", methods=['POST'])
 @login_required
 def delete(blogpost_id):
     blogpost = Blogpost.query.get_or_404(blogpost_id)
     db.session.delete(blogpost)
     db.session.commit()
-    return redirect(url_for("app.index"))
+    return redirect(url_for("index"))
 
 
-@auth.route('/logout')
+@app.route('/logout')
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('app.index'))
+    return redirect(url_for('index'))
+
+
+@app.route("/")
+def index():
+    date = datetime.now()
+    blogposts = Blogpost.query.order_by(Blogpost.date_posted.desc()).all()
+    return render_template("index.html", blogposts=blogposts, date=date)
+
+
+@app.route('/<int:blogpost_id>/')
+def blogpost(blogpost_id):
+    blogpost = Blogpost.query.get_or_404(blogpost_id)
+    return render_template('blogpost.html', blogpost=blogpost)
+
+
+if __name__ == '__main__':
+    app.run()
